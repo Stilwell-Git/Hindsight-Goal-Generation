cd6fa1621635dada03b46d41045359e6407b8f32
Stilwell <846046796@qq.com>
2020-01-07 21:38:46 +0800

    fix bugs in normal-learner

--- learner/normal.py
+++ learner/normal.py
@@ -24,24 +24,3 @@ class NormalLearner:
 					info = agent.train(buffer.sample_batch())
 					args.logger.add_dict(info)
 				agent.target_update()
-
-		for _ in range(args.test_rollouts):
-			def test_rollout(env, prefix=''):
-				rewards = 0.0
-				obs = env.reset()
-				for timestep in range(args.timesteps):
-					action, info = agent.step(obs, explore=False, test_info=True)
-					args.logger.add_dict(info, prefix)
-					obs, reward, done, info = env.step(action)
-					rewards += reward
-					if timestep==args.timesteps-1: done = True
-					if done: break
-				args.logger.add_dict(info, prefix)
-
-			if args.goal_based:
-				# goal-based envs test
-				test_rollout(env, 'train/')
-				test_rollout(env_test, 'test/')
-			else:
-				# normal envs test
-				test_rollout(env)
